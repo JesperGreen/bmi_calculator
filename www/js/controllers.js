75d93401c629be787e71f6623631c0aa12168ce1
Jesper <jesper_gs_@hotmail.com>
2016-09-20 02:14:45 +0200

    defined the calculateBMI function in controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,3 +7,15 @@ bmi.controller('AboutController', function () {
 bmi.controller('BmiController', function() {
 
 });
+
+bmi.controller('BmiController', function($scope) {
+  $scope.data = {};
+  $scope.calculateBMI = function() {
+    var person = new Person({
+      weight: $scope.data.weight,
+      height: $scope.data.height
+    });
+    person.calculate_bmi_met();
+    $scope.person = person;
+  };
+});
