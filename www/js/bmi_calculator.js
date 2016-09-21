af8a01add70f74c412f9a43ff0625f852a59a374
Jesper <jesper_gs_@hotmail.com>
2016-09-21 07:38:28 +0200

    rewrote calc / person.js to match the code from the extras chapter

--- www/js/bmi_calculator.js
+++ www/js/bmi_calculator.js
@@ -12,28 +12,25 @@ BMICalculator.prototype.metric_bmi = function(obj) {
 };
 
 BMICalculator.prototype.imperial_bmi = function(obj) {
-  var weight = obj.weight_in_lbs;
-  var height_in_feet = obj.height_in_feet * 12;
-  var height_in_inches = obj.height_in_inches;
-  var height = height_in_feet + height_in_inches;
+  var weight = obj.weight;
+  var height = obj.height;
   if (weight > 0 && height > 0) {
-    var finalBmi = (weight * 703) / (height * height);
+    var finalBmi = (weight * 703 ) / (height * height);
     obj.bmiValue =  parseFloat(finalBmi.toFixed(2));
     setBMIMessage(obj);
   }
 };
-
 function setBMIMessage (obj, value){
   if (obj.bmiValue < 18.5) {
     obj.bmiMessage = "Underweight"
   }
-  if (obj.bmiValue >= 18.5 && obj.bmiValue < 25) {
+  if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
     obj.bmiMessage = "Normal"
   }
-  if (obj.bmiValue >= 25 && obj.bmiValue < 30) {
+  if (obj.bmiValue > 25 && obj.bmiValue < 30) {
     obj.bmiMessage = "Overweight"
   }
-  if (obj.bmiValue >= 30) {
+  if (obj.bmiValue > 30) {
     obj.bmiMessage = "Obese"
   }
-};
+}
--- www/js/person.js
+++ www/js/person.js
@@ -1,21 +1,14 @@
 function Person(attr) {
   this.weight = attr.weight;
-  this.weight_in_lbs = attr.weight_in_lbs;
   this.height = attr.height;
-  this.height_in_inches = attr.height_in_inches;
-  this.height_in_feet = attr.height_in_feet;
-  this.system = attr.system;
 };
 
-Person.prototype.calculate_bmi = function() {
+Person.prototype.calculate_bmi_met = function() {
   calculator = new BMICalculator();
-  if (this.system == 'metric') {
-    calculator.metric_bmi(this);
-  }
-  else if (this.system == 'imperial') {
-    calculator.imperial_bmi(this);
-  }
-  else {
-    alert("You need to enter your height and weight")
-  };
+  calculator.metric_bmi(this);
+};
+
+Person.prototype.calculate_bmi_imp = function() {
+  calculator = new BMICalculator();
+  calculator.imperial_bmi(this);
 };
